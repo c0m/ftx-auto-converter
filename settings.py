15b145bf821e94c57f5231636621580465256c67
c0m <61923663+c0m@users.noreply.github.com>
2021-09-26 17:23:05 +0100

    Fix typo in settings

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 API = ''
 SECRET = ''
-SUBACCOUNT = None
-TARGET_TO_COINS = '' # Ticker of target currency you want to convert to, e.g. 'BTC', 'SOL', 'USD', etc..
+SUBACCOUNT = '' #do I need this if it's a special api key for the subaccount? Guess we'll find out
+TARGET_TO_COIN = '' # Ticker of target currency you want to convert to, e.g. 'BTC', 'SOL', 'USD', etc..
 TARGET_FROM_COINS = [''] # Ticker of currencies you want to convert from as an array, e.g. ['SRM', 'GBP', 'SOL', 'USDT']
