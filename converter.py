0e6aae112908347004413f44438e5674f94ea6e0
c0m <61923663+c0m@users.noreply.github.com>
2021-09-26 17:18:31 +0100

    Make coins name more consistent

--- converter.py
+++ converter.py
@@ -1,9 +1,13 @@
 from ftx.client import FtxClient
-from settings import TARGET_TO_CURRENCY, TARGET_FROM_CURRENCIES
+from settings import TARGET_TO_COINS, TARGET_FROM_COINS
 
 def auto_converter(client: FtxClient) -> None:
-    for coin in TARGET_FROM_CURRENCIES: 
-        balance = client.get_balance_coin(coin).get('availableWithoutBorrow')
-        if balance > 0: 
-            print("converty")
+    for coin in TARGET_FROM_COINS: 
+        if decide_to_convert(client, coin):
+            print("True")
     
+def decide_to_convert(client: FtxClient, coin: str):
+    balance = client.get_balance_coin(coin).get('availableWithoutBorrow')
+    if balance > 0.000001: 
+        return True
+    return False
