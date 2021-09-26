0879ca4746568dd8a61115734f71d2aff93a9537
c0m <61923663+c0m@users.noreply.github.com>
2021-09-26 17:28:25 +0100

    Add ability to convert currencies if the balance is above a certain small amount

--- converter.py
+++ converter.py
@@ -1,13 +1,13 @@
 from ftx.client import FtxClient
-from settings import TARGET_TO_COINS, TARGET_FROM_COINS
+from settings import TARGET_TO_COIN, TARGET_FROM_COINS
+MIN_BALANCE_TO_CONVERT = 0.000001
 
 def auto_converter(client: FtxClient) -> None:
-    for coin in TARGET_FROM_COINS: 
-        if decide_to_convert(client, coin):
-            print("True")
-    
-def decide_to_convert(client: FtxClient, coin: str):
-    balance = client.get_balance_coin(coin).get('availableWithoutBorrow')
-    if balance > 0.000001: 
-        return True
-    return False
+    for coin in TARGET_FROM_COINS:    
+        balance = client.get_balance_coin(coin).get('availableWithoutBorrow')
+        if balance > MIN_BALANCE_TO_CONVERT:
+            convert_coin(client, coin, balance)
+
+def convert_coin(client: FtxClient, from_coin: str, size: float):
+    current_quote_request = client.post_quote_request(from_coin, TARGET_TO_COIN, 0.001)
+    client.post_accept_quote(current_quote_request['quoteId'])
